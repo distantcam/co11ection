c31e7d01d97146d064706e265446ef7e94f0d7c1
Cameron MacFarland <distantcam@gmail.com>
2022-03-11 13:15:20 +0800

    Cleaning up structure

--- .eleventy.js
+++ .eleventy.js
@@ -1,21 +1,16 @@
 const fs = require("fs");
-const htmlmin = require("html-minifier");
 
 module.exports = function (eleventyConfig) {
-  if (process.env.ELEVENTY_PRODUCTION) {
-    eleventyConfig.addTransform("htmlmin", htmlminTransform);
-  } else {
-    eleventyConfig.setBrowserSyncConfig({
-      callbacks: { ready: browserSyncReady },
-    });
-  }
+  // Transforms
+  eleventyConfig.addTransform("html", require("./src/_11ty/html-transform"));
 
   // Passthrough
-  eleventyConfig.addPassthroughCopy({ "src/static": "." });
+  eleventyConfig.addPassthroughCopy({ "src/assets/static": "." });
 
   // Watch targets
-  eleventyConfig.addWatchTarget("./src/styles/");
+  eleventyConfig.addWatchTarget("./src/assets/");
 
+  // GutHub hosting support
   var pathPrefix = "";
   if (process.env.GITHUB_REPOSITORY) {
     pathPrefix = process.env.GITHUB_REPOSITORY.split("/")[1];
@@ -28,27 +23,3 @@ module.exports = function (eleventyConfig) {
     pathPrefix,
   };
 };
-
-function browserSyncReady(err, bs) {
-  bs.addMiddleware("*", (req, res) => {
-    const content_404 = fs.readFileSync("_site/404.html");
-    // Provides the 404 content without redirect.
-    res.write(content_404);
-    // Add 404 http status code in request header.
-    // res.writeHead(404, { "Content-Type": "text/html" });
-    res.writeHead(404);
-    res.end();
-  });
-}
-
-function htmlminTransform(content, outputPath) {
-  if (outputPath.endsWith(".html")) {
-    let minified = htmlmin.minify(content, {
-      useShortDoctype: true,
-      removeComments: true,
-      collapseWhitespace: true,
-    });
-    return minified;
-  }
-  return content;
-}
